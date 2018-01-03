a948c424e2e162e0b4abda8a74ca38ac4c32b9c8
Charles Baker <charles.baker@sweetsoftware.co.nz>
2018-01-03 17:26:03 +1300

    Update build.lua to work with latest Sweet Build

---
+++ src/build.lua
@@ -0,0 +1,43 @@
+
+require "build";
+require "build.cc";
+require "build.Parser";
+require "build.macos";
+require "build.windows";
+
+platform = platform or build:operating_system();
+variant = build:lower( variant or 'debug' );
+version = version or ('%s %s %s'):format( os.date('%Y.%m.%d %H:%M:%S'), platform, variant );
+goal = goal or "";
+jobs = jobs or 8;
+
+local settings = build:initialize {
+    variants = { "debug", "release", "shipping" };
+    bin = build:root( ("../%s/bin"):format(variant) );
+    lib = build:root( ("../%s/lib"):format(variant) );
+    obj = build:root( ("../%s/obj"):format(variant) );
+    include_directories = {
+        build:root()
+    };
+    library_directories = {
+        build:root( ("../%s/lib"):format(variant) ),
+    };
+    sln = build:root( "../sweet_render.sln" );
+    xcodeproj = build:root( "../sweet_render.xcodeproj" );
+};
+
+build:default_targets {
+    'sweet/render/render_examples',
+    'sweet/render/render_test'
+};
+
+buildfile "jpeg/jpeg.build";
+buildfile "png/png.build";
+buildfile "sweet/assert/assert.build";
+buildfile "sweet/atomic/atomic.build";
+buildfile "sweet/error/error.build";
+buildfile "sweet/math/math.build";
+buildfile "sweet/pointer/pointer.build";
+buildfile "sweet/render/render.build";
+buildfile "unit/unit.build";
+buildfile "zlib/zlib.build";
