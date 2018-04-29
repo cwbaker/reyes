163a13faec79c34e6022c4d6de059dbc491c63bf
Charles Baker <charles.baker@sweetsoftware.co.nz>
2018-04-29 13:28:10 +1200

    Use build scripts with explicit libraries, move src/build.lua to build.lua

--- build.lua
+++ build.lua
@@ -1,5 +1,5 @@
 
-package.path = ('%s;%s;%s'):format( package.path, build:root('lalr/lalr/?.lua'), build:root('lalr/lalr/?/init.lua') );
+package.path = ('%s;%s;%s'):format( package.path, build:root('src/lalr/lalr/?.lua'), build:root('src/lalr/lalr/?/init.lua') );
 
 require "build";
 require "build.cc";
@@ -17,49 +17,49 @@ jobs = jobs or 8;
 
 local settings = build:initialize {
     variants = { "debug", "release", "shipping" };
-    bin = build:root( ("../%s/bin"):format(variant) );
-    lib = build:root( ("../%s/lib"):format(variant) );
-    obj = build:root( ("../%s/obj"):format(variant) );
+    bin = build:root( ('%s/bin'):format(variant) );
+    lib = build:root( ('%s/lib'):format(variant) );
+    obj = build:root( ('%s/obj'):format(variant) );
     include_directories = {
-        build:root(),
-        build:root( 'zlib' ),
-        build:root( 'png' ),
-        build:root( 'unittest-cpp' );
-        build:root( 'lalr/lalr' );
+        build:root( 'src' ),
+        build:root( 'src/zlib' ),
+        build:root( 'src/png' ),
+        build:root( 'src/unittest-cpp' );
+        build:root( 'src/lalr/lalr' );
     };
     library_directories = {
-        build:root( ("../%s/lib"):format(variant) ),
+        build:root( ("%s/lib"):format(variant) ),
     };
     visual_studio = {
-        sln = build:root( "../sweet_render.sln" );
+        sln = build:root( "sweet_render.sln" );
     };
     xcode = {
-        xcodeproj = build:root( "../sweet_render.xcodeproj" );
+        xcodeproj = build:root( "sweet_render.xcodeproj" );
     };
     lalr = {
         lalrc = build:switch {
             build:operating_system();
-            linux = build:root( '../%s/bin/lalrc' );
-            macos = build:root( '../%s/bin/lalrc' );
-            windows = build:root( '../%s/bin/lalrc.exe' );
+            linux = build:root( '%s/bin/lalrc' );
+            macos = build:root( '%s/bin/lalrc' );
+            windows = build:root( '%s/bin/lalrc.exe' );
         };
     };
 };
 
 build:default_targets {
-    'lalr/lalr/lalrc',
-    'sweet/render/render_examples',
-    'sweet/render/render_test'
+    'src/lalr/lalr/lalrc',
+    'src/sweet/render/render_examples',
+    'src/sweet/render/render_test'
 };
 
-buildfile 'lalr/lalr/lalr.build';
-buildfile "jpeg/jpeg.build";
-buildfile "png/png.build";
-buildfile "sweet/assert/assert.build";
-buildfile "sweet/atomic/atomic.build";
-buildfile "sweet/error/error.build";
-buildfile "sweet/math/math.build";
-buildfile "sweet/pointer/pointer.build";
-buildfile "sweet/render/render.build";
-buildfile "unittest-cpp/unittest-cpp.build";
-buildfile "zlib/zlib.build";
+buildfile 'src/lalr/lalr/lalr.build';
+buildfile 'src/jpeg/jpeg.build';
+buildfile 'src/png/png.build';
+buildfile 'src/sweet/assert/assert.build';
+buildfile 'src/sweet/atomic/atomic.build';
+buildfile 'src/sweet/error/error.build';
+buildfile 'src/sweet/math/math.build';
+buildfile 'src/sweet/pointer/pointer.build';
+buildfile 'src/sweet/render/render.build';
+buildfile 'src/unittest-cpp/unittest-cpp.build';
+buildfile 'src/zlib/zlib.build';
