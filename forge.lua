8c9383eefcbe45126a6be254afc171f2ffd7ebf1
Charles Baker <charles@sweetsoftware.nz>
2022-01-19 20:02:48 +1300

    Compile Windows with C++17 for inline variables in math/constants.hpp

--- forge.lua
+++ forge.lua
@@ -52,13 +52,6 @@ local cc = require 'forge.cc' {
     warnings_as_errors = true;
 };
 
--- Bump the C++ standard to c++14 when building on Windows as that is the 
--- closest standard supported by Microsoft Visual C++.
-local settings = cc.settings;
-if settings.platform == 'windows' then
-    settings.standard = 'c++14';
-end
-
 local lalr = require 'forge.lalr';
 cc:install( lalr );
 
